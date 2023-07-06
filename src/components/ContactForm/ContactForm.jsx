217595530f27d0a06e98cd27f512b45b74a0f1c4
Iuliia23 <117689978+Iuliia23@users.noreply.github.com>
2023-07-07 00:32:58 +0200

    Create src (components, redux)

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { nanoid } from '@reduxjs/toolkit';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from 'redux/contactsSlice';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { getContacts } from 'redux/selectors';
+import PropTypes from 'prop-types';
+import { Form, Label, Input, Button } from './ContactForm.styled';
+
+const notify = {
+  error: message => toast.error(message),
+  success: message => toast.success(message),
+};
+
+const ContactForm = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    if (name.trim() !== '' && number.trim() !== '') {
+      const isExistingContact = contacts.some(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      );
+
+      if (isExistingContact) {
+        toast.error(`${name} is already in contacts`);
+        return;
+      }
+
+      const newContact = {
+        id: nanoid(),
+        name: name.trim(),
+        number: number.trim(),
+      };
+
+      dispatch(addContact(newContact));
+      setName('');
+      setNumber('');
+
+      notify.success(`${newContact.name} added to contacts`);
+    }
+  };
+
+  return (
+    <section>
+      <ToastContainer />
+      <Form onSubmit={handleSubmit}>
+        <Label htmlFor="name">Name:</Label>
+        <Input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я])?[a-zA-Zа-яА-Я]*)*$"
+          required
+          value={name}
+          onChange={handleChange}
+        />
+        <Label htmlFor="number">Number:</Label>
+        <Input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
+          required
+          value={number}
+          onChange={handleChange}
+        />
+        <br />
+        <Button type="submit">Add Contact</Button>
+      </Form>
+    </section>
+  );
+};
+
+ContactForm.propTypes = {
+  name: PropTypes.string,
+  number: PropTypes.string,
+  onSubmit: PropTypes.func,
+};
+
+export default ContactForm;
